800cdc26e32d91224c629bca357e33578b9d0873
Majorbandzz <go59@student.le.ac.uk>
2025-02-09 18:13:35 +0000

    Making sure that my python works with flask

--- credit_scoring_backend/app.py
+++ credit_scoring_backend/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:hotdog123@localhost/credit_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 @app.route('/')
 def home():
-    return jsonify({"message": "Credit Scoring API Running!"})
+    return jsonify({"message": "Credit Scoring API is Running with MySQL!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
