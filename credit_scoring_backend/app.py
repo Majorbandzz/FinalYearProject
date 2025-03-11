39e4e5732d9cddf0150783a858905a42fc64465d
Majorbandzz <go59@student.le.ac.uk>
2025-03-11 20:35:21 +0000

    Edited the message displayed on the frontend

--- credit_scoring_backend/app.py
+++ credit_scoring_backend/app.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import db 
+from flask_bcrypt import Bcrypt 
+from models import db, bcrypt  
 from routes import user_blueprint
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Synergy Credit Scoring Tool!"
+    return "Welcome to The Synergy Credit Scoring Tool!"
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hotdog123@localhost/credit_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+bcrypt.init_app(app)  
 
 app.register_blueprint(user_blueprint, url_prefix='/users')
 
