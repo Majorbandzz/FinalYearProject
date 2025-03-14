4c22add70496b4f74675e48986b99cee0b6b5234
Majorbandzz <go59@student.le.ac.uk>
2025-03-14 22:48:14 +0000

    created this js file to design my frontend

--- credit-scoring-frontend/src/App.js
+++ credit-scoring-frontend/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to The Synergy Credit Scoring App</h1>
+    <div className="text-center p-4">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Gerard!</h1>
+      <p className="text-lg">Your React App is running smoothly please proceed.</p>
     </div>
   );
 }
